bcd03780daf656b1c76dc2cd978c43279b264b6d
Manfredi Di Fazio <LA_TUA_EMAIL_GITHUB>
2026-07-12 11:40:57 +0200

    Use VITE_API_URL env variable for production API calls

--- frontend/src/services/accountStatementService.js
+++ frontend/src/services/accountStatementService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000" });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL ?? "" });
 
 function notifyAccountStatementsUpdated() {
   window.dispatchEvent(new CustomEvent("mgcf:account-statements-updated"));
